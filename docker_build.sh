1d0d9c3f282f4249971c29077b4fae6b405ea45f
Albertin Loic <loic.albertin@atos.net>
2016-06-29 09:44:15 +0200

    Refactor docker image

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${script_dir}
+
+if [[ ! -e ${script_dir}/janus ]]; then
+    cd ${script_dir}
+    make
+fi
+cp ${script_dir}/janus ${script_dir}/pkg/
+cd ${script_dir}/pkg
 docker build -t starlings/janus .
