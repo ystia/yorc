2f04e52396de55a03c10bf09267dc0d5e6b8f67d
Loic Pauletto <loic.pauletto@atos.net>
2016-07-08 16:39:04 +0200

    Add imports

---
+++ tosca/import_test.go
@@ -0,0 +1,32 @@
+package tosca
+
+import (
+	"gopkg.in/yaml.v2"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestImportDefinitionConcrete_UnmarshalYAML_SimpleGrammar(t *testing.T) {
+
+	var data = `
+imports:
+  - some_definition_file: path1/path2/some_defs.yaml
+  - another_definition_file:
+      file: path1/path2/file2.yaml
+      repository: my_service_catalog
+      namespace_uri: http://mycompany.com/tosca/1.0/platform
+      namespace_prefix: mycompany
+`
+	importMap := ImportMapInteface{}
+	err := yaml.Unmarshal([]byte(data), &importMap)
+	if err == nil {
+		assert.Len(t, importMap, 1)
+		assert.Contains(t, importMap, "imports")
+		importDef := importMap["imports"]
+		assert.Len(t, importDef, 2)
+		importDefMap := importDef[0]
+		assert.Contains(t, importDefMap, "some_definition_file")
+		importDefMap = importDef[1]
+		assert.Contains(t, importDefMap, "another_definition_file")
+	}
+}
---
+++ tosca/imports.go
@@ -0,0 +1,34 @@
+package tosca
+
+type ImportMapInteface map[string][]map[string]ImportDefinitionBody
+
+type ImportMap map[string]ImportDefinitionBody
+type ImportDefinitionBody struct {
+	File			string			`yaml:"file,omitempty"`
+	Repository    		string                 	`yaml:"repository,omitempty"`
+	NamespaceURI 		string             	`yaml:"namespace_uri,omitempty"`
+	NamespacePrefix 	string             	`yaml:"namespace_prefix,omitempty"`
+}
+
+func (i *ImportDefinitionBody) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var s string
+
+	if err := unmarshal(&s); err == nil {
+		i.File = s
+		return nil
+	}
+	var str struct {
+		File			string 			`yaml:"file,omitempty"`
+		Repository    		string                 	`yaml:"repository,omitempty"`
+		NamespaceURI 		string             	`yaml:"namespace_uri,omitempty"`
+		NamespacePrefix 	string             	`yaml:"namespace_prefix,omitempty"`
+	}
+	if err := unmarshal(&str); err != nil {
+		return err
+	}
+	i.File = str.File
+	i.Repository = str.Repository
+	i.NamespaceURI = str.NamespaceURI
+	i.NamespacePrefix = str.NamespacePrefix
+	return nil
+}
