39c4de673596e551d36c0ad59134cf3aaf943ad6
Loic Pauletto <loic.pauletto@atos.net>
2016-07-08 16:40:35 +0200

    Fix ansible execution test

--- prov/ansible/execution_test.go
+++ prov/ansible/execution_test.go
@@ -5,12 +5,20 @@ import (
 	"os"
 	"testing"
 	"text/template"
+	"path/filepath"
 )
 
 func TestTemplates(t *testing.T) {
 	e := &execution{Inputs: []string{"A: 1", "B: 2", "C: 3"}, NodeName: "Welcome", Operation: "tosca.interfaces.node.lifecycle.Standard.start", Artifacts: map[string]string{"scripts": "my_scripts"}, OverlayPath: "/some/local/path"}
+
+	funcMap := template.FuncMap{
+		// The name "path" is what the function will be called in the template text.
+		"path": filepath.Dir,
+	}
+
 	tmpl := template.New("execTest")
 	tmpl = tmpl.Delims("[[[", "]]]")
+	tmpl = tmpl.Funcs(funcMap)
 	tmpl, err := tmpl.Parse(ansible_playbook)
 	assert.Nil(t, err)
 	err = tmpl.Execute(os.Stdout, e)
