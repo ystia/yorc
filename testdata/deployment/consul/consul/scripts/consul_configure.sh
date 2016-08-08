ccbf36c76699b5669627474f40ae8451c34f3c34
Loic Pauletto <loic.pauletto@atos.net>
2016-08-08 13:22:41 +0200

    Add consul type test

---
+++ testdata/deployment/consul/consul/scripts/consul_configure.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+. ${utils_scripts}/utils.sh
+
+INSTALL_DIR=$(eval readlink -f "${INSTALL_DIR}")
+
+
+if [[ ! -e ${INSTALL_DIR}/work/.agentmode ]]; then
+    log info "Configuring Consul to run in server mode"
+    # We are not configured to connect to a server so let's start as a server by our own
+    addresses=( $(get_multi_instances_attribute "IP_ADDRESS") )
+    number_of_masters=${#addresses[@]}
+    # quote array values TODO check if we can use a better  solution to do it
+    addresses=( ${addresses[@]/#/\"} )
+    addresses=( ${addresses[@]/%/\"} )
+    addr_block="$(join_list "," ${addresses[*]})"
+    sed -i -e "/client_addr/ a\  \"server\": true,\n\
+  \"bootstrap_expect\": ${number_of_masters},\n\
+  \"retry_join\": [${addr_block}]," ${INSTALL_DIR}/config/1_main_conf.json
+else
+    # In agent mode the preconfigure_source script already setup the connection to masters
+    log info "Consul configured to run in agent mode"
+fi
