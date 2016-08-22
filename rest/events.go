982c78f21d26e28d5e19e2fe2199afed4ea85655
Loic Pauletto <loic.pauletto@atos.net>
2016-08-22 11:35:39 +0200

    Add node event support
    Improvement in progress

--- rest/events.go
+++ rest/events.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/context"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"novaforge.bull.com/starlings-janus/janus/deployments"
 	"novaforge.bull.com/starlings-janus/janus/events"
 	"novaforge.bull.com/starlings-janus/janus/log"
 	"strconv"
@@ -32,7 +33,7 @@ func (s *Server) pollEvents(w http.ResponseWriter, r *http.Request) {
 			WriteError(w, NewBadRequestParameter("index", err))
 			return
 		}
-		if timeout > 10 * time.Minute {
+		if timeout > 10*time.Minute {
 			timeout = 10 * time.Minute
 		}
 	}
@@ -43,6 +44,51 @@ func (s *Server) pollEvents(w http.ResponseWriter, r *http.Request) {
 	}
 
 	eventsCollection := EventsCollection{Events: events, LastIndex: lastIdx}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(eventsCollection)
+}
+
+func (s *Server) pollNodeEvents(w http.ResponseWriter, r *http.Request) {
+	var params httprouter.Params
+	params = context.Get(r, "params").(httprouter.Params)
+	id := params.ByName("id")
+	name := params.ByName("name")
+	sub := events.NewSubscriber(s.consulClient.KV(), id)
+	values := r.URL.Query()
+	var err error
+	var waitIndex uint64 = 1
+	var timeout time.Duration = 5 * time.Minute
+	if idx := values.Get("index"); idx != "" {
+		if waitIndex, err = strconv.ParseUint(idx, 10, 64); err != nil {
+			WriteError(w, NewBadRequestParameter("index", err))
+			return
+		}
+	}
+
+	if dur := values.Get("wait"); dur != "" {
+		if timeout, err = time.ParseDuration(dur); err != nil {
+			WriteError(w, NewBadRequestParameter("index", err))
+			return
+		}
+		if timeout > 10*time.Minute {
+			timeout = 10 * time.Minute
+		}
+	}
+
+	events, lastIdx, err := sub.NewEvents(waitIndex, timeout)
+	if err != nil {
+		log.Panicf("Can't retrieve events: %v", err)
+	}
+
+	var res []deployments.Event
+	for _, v := range events {
+		if v.Node == name {
+			res = append(res, v)
+		}
+	}
+
+	eventsCollection := EventsCollection{Events: res, LastIndex: lastIdx}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(eventsCollection)
