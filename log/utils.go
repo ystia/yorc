aa33838e6e2590e0af022fd4775c96e2c9491a32
Loic Pauletto <loic.pauletto@atos.net>
2016-09-15 15:21:53 +0200

    Add comments

--- log/utils.go
+++ log/utils.go
@@ -63,32 +63,47 @@ func checkErr(err error) {
 
 //This function flush the buffer and write the content on Consul
 func (b *BufferedConsulWriter) FlushSoftware() error {
+
+	//Construct the JSON from the buffer
 	v, err := jason.NewObjectFromBytes(b.buf)
 	if err != nil {
 		panic(err)
 	}
 
+	//Get the array of object of plays
 	plays, err := v.GetObjectArray("plays")
 	for _, data := range plays {
+		//Extract the tasks from the play
 		tasks, err := data.GetObjectArray("tasks")
 		checkErr(err)
 		for _, host := range tasks{
+			//Extract the hosts object from the  tasks
 			tmp, err := host.GetObject("hosts")
 			checkErr(err)
+			//Convert the host into map like ["IP_ADDR"]Json_Object
 			mapTmp := tmp.Map()
+			//Iterate on this map (normally a single object)
 			for k, v := range mapTmp{
-				tmp2, err := v.Object()
+				//Convert the value in Object type
+				obj, err := v.Object()
 				checkErr(err)
-				if ok, err := tmp2.GetBoolean("failed"); ok {
+				//Check if a failed field is present
+				if ok, err := obj.GetBoolean("failed"); ok {
 					checkErr(err)
-					str, err := tmp2.GetString("msg")
+					//Get Error message
+					str, err := obj.GetString("msg")
 					checkErr(err)
+					//Display it and store it in consul
+					//TODO: May interesting to store Host (IP Address) on consul, currently not done
 					Debugf("Error found on host : %s  message : %s",k, str)
 					kv := &api.KVPair{Key: filepath.Join(b.prefix, b.depId, "logs", SOFTWARE_LOG_PREFIX+"__"+time.Now().Format(time.RFC3339Nano)), Value: []byte(str)}
 					_, err = b.kv.Put(kv, nil)
 					checkErr(err)
 				}
-				if std, err := tmp2.GetString("stdout"); err == nil {
+				//Check if a stdout field is present (The stdout field is exported for shell tasks on ansible)
+				if std, err := obj.GetString("stdout"); err == nil {
+					//Display it and store it in consul
+					//TODO: May interesting to store Host (IP Address) on consul, currently not done
 					Debugf("Stdout found on host : %s  message : %s",k, std)
 					kv := &api.KVPair{Key: filepath.Join(b.prefix, b.depId, "logs", SOFTWARE_LOG_PREFIX+"__"+time.Now().Format(time.RFC3339Nano)), Value: []byte(std)}
 					_, err = b.kv.Put(kv, nil)
