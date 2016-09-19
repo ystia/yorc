b4e6a8c49ab15d62629b077c8fd791a857bc69b9
Albertin Loic <loic.albertin@atos.net>
2016-09-19 14:28:18 +0200

    BRBDCF-11 Multi-instances support:
    Add lifecycle demo

---
+++ testdata/deployment/lifecycle/demo-lifecycle/1.0.0-SNAPSHOT/expanded/scripts/Generic/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+env_file=/tmp/$$.env
+printenv > $env_file
+wget --no-proxy --timeout=30 -S -q "http://a4c_registry/log_node_operation.php?node=$NODE&instance=$INSTANCE&operation=stop" --post-file=$env_file
+exit 0
