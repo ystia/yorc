2384b460b73d1c5a2643ef569c20cc17e07c1dcd
Albertin Loic <loic.albertin@atos.net>
2016-10-03 10:28:45 +0200

    Few changes on XFS storage

--- testdata/deployment/FS/extended/scripts/mkfs.sh
+++ testdata/deployment/FS/extended/scripts/mkfs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
 volume_id=${VOLUME_ID}
-fs_type=${FS_TYPE}
 filesys=${PARTITION_NAME}
 
-(eval $(blkid $DEV | awk ' { print $3 } '); echo $TYPE)
+(eval $(sudo blkid ${filesys} | awk ' { print $3 } '); echo $TYPE)
 
 if [ -z "${volume_id}" ]; then
-    mkfs_executable='mkfs.xfs'
 
     echo "Creating xfs file system using ${mkfs_executable}"
-    sudo ${mkfs_executable} ${filesys}
+    sudo mkfs.xfs -f ${filesys}
 else
     echo "Not making a filesystem since 'volume_id' not empty"
 fi
