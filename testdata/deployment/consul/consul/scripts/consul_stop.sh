4a15b507d6d2138a558102b8e15d3dbc9c141f4f
Albertin Loic <loic.albertin@atos.net>
2016-10-04 14:48:34 +0200

    BRBDCF-27: Network nodes support
    Refactoring testdata

--- testdata/deployment/consul/consul/scripts/consul_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-INSTALL_DIR=$(eval readlink -f "${INSTALL_DIR}")
-. ${utils_scripts}/utils.sh
-
-log info "Gracefully leaving consul cluster"
-${INSTALL_DIR}/consul leave
-exit $?
