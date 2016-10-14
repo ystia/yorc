72b52550e1a102cee00f33a500f2176bc543a70a
Albertin Loic <loic.albertin@atos.net>
2016-10-14 14:49:31 +0200

    Support requirements definition in Alien specific format

--- tosca/requirements_test.go
+++ tosca/requirements_test.go
@@ -2,7 +2,9 @@ package tosca
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
+	"novaforge.bull.com/starlings-janus/janus/log"
 	"testing"
 )
 
@@ -11,11 +13,17 @@ func TestGroupedRequirementParallel(t *testing.T) {
 		t.Run("TestRequirementAssignment_Complex", requirementAssignment_Complex)
 		t.Run("TestRequirementAssignment_Simple", requirementAssignment_Simple)
 		t.Run("TestRequirementAssignment_SimpleRelationship", requirementAssignment_SimpleRelationship)
+		t.Run("TestRequirementDefinition_Standard", requirementDefinition_Standard)
+		t.Run("TestRequirementDefinition_Alien", requirementDefinition_Alien)
 	})
 }
 
 type ReqTestNode struct {
 	Requirements []RequirementAssignmentMap `yaml:"requirements,omitempty"`
+}
+
+type ReqDefTestNode struct {
+	Requirements []RequirementDefinitionMap `yaml:"requirements,omitempty"`
 }
 
 func requirementAssignment_Complex(t *testing.T) {
@@ -80,4 +88,101 @@ func requirementAssignment_SimpleRelationship(t *testing.T) {
 	assert.Equal(t, "nodeName", req.Node)
 	assert.Equal(t, "tosca.capabilities.cap", req.Capability)
 	assert.Equal(t, "tosca.relationships.rs", req.Relationship)
+}
+
+func requirementDefinition_Standard(t *testing.T) {
+	t.Parallel()
+	log.SetDebug(true)
+	data := `NodeType:
+    requirements:
+      - server_endpoint:
+          capability: starlings.capabilities.ConsulServer
+          relationship_type: starlings.relationships.ConnectsConsulAgentToServer
+          occurrences: [ 0, 1 ]
+      - wan_endpoint:
+          capability: starlings.capabilities.ConsulServerWAN
+          relationship_type: starlings.relationships.ConnectsConsulServerWAN
+          occurrences: [ 0, UNBOUNDED ]
+      - simple_endpoint: starlings.capabilities.Simple
+        `
+
+	nodes := make(map[string]ReqDefTestNode)
+	err := yaml.Unmarshal([]byte(data), &nodes)
+
+	log.Printf("%+v", nodes)
+
+	require.Nil(t, err)
+	require.Contains(t, nodes, "NodeType")
+	node := nodes["NodeType"]
+
+	require.Len(t, node.Requirements, 3)
+
+	require.Contains(t, node.Requirements[0], "server_endpoint")
+	req := node.Requirements[0]["server_endpoint"]
+
+	require.Equal(t, "starlings.capabilities.ConsulServer", req.Capability)
+	require.Equal(t, "starlings.relationships.ConnectsConsulAgentToServer", req.Relationship)
+	require.Equal(t, uint64(0), req.Occurrences.LowerBound)
+	require.Equal(t, uint64(1), req.Occurrences.UpperBound)
+
+	require.Contains(t, node.Requirements[1], "wan_endpoint")
+	req = node.Requirements[1]["wan_endpoint"]
+
+	require.Equal(t, "starlings.capabilities.ConsulServerWAN", req.Capability)
+	require.Equal(t, "starlings.relationships.ConnectsConsulServerWAN", req.Relationship)
+	require.Equal(t, uint64(0), req.Occurrences.LowerBound)
+	require.Equal(t, uint64(UNBOUNDED), req.Occurrences.UpperBound)
+
+	require.Contains(t, node.Requirements[2], "simple_endpoint")
+	req = node.Requirements[2]["simple_endpoint"]
+
+	require.Equal(t, "starlings.capabilities.Simple", req.Capability)
+	require.Equal(t, "", req.Relationship)
+	require.Equal(t, uint64(0), req.Occurrences.LowerBound)
+	require.Equal(t, uint64(0), req.Occurrences.UpperBound)
+
+}
+
+func requirementDefinition_Alien(t *testing.T) {
+	t.Parallel()
+	log.SetDebug(true)
+	data := `NodeType:
+    requirements:
+      - server_endpoint: starlings.capabilities.ConsulServer
+        relationship_type: starlings.relationships.ConnectsConsulAgentToServer
+        lower_bound: 0
+        upper_bound: 1
+      - wan_endpoint: starlings.capabilities.ConsulServerWAN
+        relationship_type: starlings.relationships.ConnectsConsulServerWAN
+        lower_bound: 0
+        upper_bound: UNBOUNDED
+        `
+
+	nodes := make(map[string]ReqDefTestNode)
+	err := yaml.Unmarshal([]byte(data), &nodes)
+
+	log.Printf("%+v", nodes)
+
+	require.Nil(t, err)
+	require.Contains(t, nodes, "NodeType")
+	node := nodes["NodeType"]
+
+	require.Len(t, node.Requirements, 2)
+
+	require.Contains(t, node.Requirements[0], "server_endpoint")
+	req := node.Requirements[0]["server_endpoint"]
+
+	require.Equal(t, "starlings.capabilities.ConsulServer", req.Capability)
+	require.Equal(t, "starlings.relationships.ConnectsConsulAgentToServer", req.Relationship)
+	require.Equal(t, uint64(0), req.Occurrences.LowerBound)
+	require.Equal(t, uint64(1), req.Occurrences.UpperBound)
+
+	require.Contains(t, node.Requirements[1], "wan_endpoint")
+	req = node.Requirements[1]["wan_endpoint"]
+
+	require.Equal(t, "starlings.capabilities.ConsulServerWAN", req.Capability)
+	require.Equal(t, "starlings.relationships.ConnectsConsulServerWAN", req.Relationship)
+	require.Equal(t, uint64(0), req.Occurrences.LowerBound)
+	require.Equal(t, uint64(UNBOUNDED), req.Occurrences.UpperBound)
+
 }
