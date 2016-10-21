a1e853f897c080a885a26c3eb858894c336c7a6e
Laurent.Gilot <gilotl@atlas.frec.bull.fr>
2016-10-21 16:08:14 +0200

    Dummy application with relationship to test a deployment using slurm

---
+++ testdata/deployment/SlurmWithRelationship/dummy/scripts/dummy_connects_agent_to_server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Starlings
+# Copyright (C) 2015-2016 Bull S.A.S. - All rights reserved
+#
+#Test if the SERVER_IP and hostname are different
+echo HPC:::$0:::${SERVER_IP}
+hostname
