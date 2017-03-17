e64eb1fd3f158a3b0ec63d3b1cf19f937421c9ef
Albertin Loic <loic.albertin@atos.net>
2017-03-17 14:59:30 +0100

    Format

--- tosca/assets.go
+++ tosca/assets.go
@@ -106,7 +106,7 @@ func janusSlurmTypesYml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "janus-slurm-types.yml", size: 450, mode: os.FileMode(436), modTime: time.Unix(1489757785, 0)}
+	info := bindataFileInfo{name: "janus-slurm-types.yml", size: 450, mode: os.FileMode(436), modTime: time.Unix(1489758280, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
