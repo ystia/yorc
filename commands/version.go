4a537756a38df85e9653bcf9b800dc1b98483d61
Albertin Loic <loic.albertin@atos.net>
2017-04-19 17:16:15 +0200

    Define a common place for managing components versions

--- commands/version.go
+++ commands/version.go
@@ -2,21 +2,32 @@ package commands
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-const version = "Janus v0.1.0"
+var version = "unknown phantom version"
+
+var gitCommit = "dev version"
 
 func init() {
+	var quiet bool
+	versionCmd := &cobra.Command{
+
+		Use:   "version",
+		Short: "Print the version",
+		Long:  `The version of Janus`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if quiet {
+				fmt.Println(version)
+			} else {
+				fmt.Println("Janus Server", version)
+				fmt.Printf("Revision: %q\n", gitCommit)
+			}
+
+		},
+	}
+	versionCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, fmt.Sprintf("Print just the release number in machine readable format (ie: %s)", version))
+
 	RootCmd.AddCommand(versionCmd)
-}
-
-var versionCmd = &cobra.Command{
-
-	Use:   "version",
-	Short: "Print the version",
-	Long:  `The version of Janus`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
-	},
 }
