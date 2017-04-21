4023bf48d8dfccd791faf0cbd18174d9f645ae6f
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-04-21 11:24:27 +0200

    fix uninstall node & update node states for runtime view

--- prov/kubernetes/executor.go
+++ prov/kubernetes/executor.go
@@ -14,6 +14,8 @@ import (
 
 	"context"
 	"strings"
+	"novaforge.bull.com/starlings-janus/janus/tasks"
+	"novaforge.bull.com/starlings-janus/janus/tosca"
 )
 
 type defaultExecutor struct {
@@ -35,12 +37,17 @@ func (e *defaultExecutor) ExecDelegate(ctx context.Context, kv *api.KV, cfg conf
 		return errors.Errorf("Unsupported node type '%s' for node '%s'", nodeType, nodeName)
 	}
 
+	instances, err := tasks.GetInstances(kv, taskID, deploymentID, nodeName)
+	if err != nil {
+		return err
+	}
+
 	op := strings.ToLower(delegateOperation)
 	switch {
 	case op == "install":
-		err = e.installNode(ctx, kv, cfg, deploymentID, nodeName)
+		err = e.installNode(ctx, kv, cfg, deploymentID, nodeName, instances)
 	case op == "uninstall":
-		err = e.uninstallNode(ctx, kv, cfg, deploymentID, nodeName)
+		err = e.uninstallNode(ctx, kv, cfg, deploymentID, nodeName, instances)
 	default:
 		return errors.Errorf("Unsupported operation %q", delegateOperation)
 	}
@@ -48,8 +55,101 @@ func (e *defaultExecutor) ExecDelegate(ctx context.Context, kv *api.KV, cfg conf
 	return err
 }
 
-func (e *defaultExecutor) installNode(ctx context.Context, kv *api.KV, cfg config.Configuration, deploymentID, nodeName string) error {
+func (e *defaultExecutor) installNode(ctx context.Context, kv *api.KV, cfg config.Configuration, deploymentID, nodeName string, instances []string) error {
+	for _, instance := range instances {
+		err := deployments.SetInstanceState(kv, deploymentID, nodeName, instance, tosca.NodeStateCreating)
+		if err != nil {
+			return err
+		}
+	}
 
+	err := e.initClientSet(cfg)
+	if err != nil {
+		return err
+	}
+
+	generator := NewGenerator(kv, cfg)
+
+	namespace, err := getNamespace(kv,deploymentID, nodeName)
+	if err != nil {
+		return err
+	}
+
+	namespace = strings.ToLower(namespace)
+	err = generator.CreateNamespaceIfMissing(deploymentID, namespace, e.clientset)
+	if err != nil {
+		return err
+	}
+
+	pod, err := generator.GeneratePod(deploymentID, nodeName)
+	if err != nil {
+		return err
+	}
+
+	_, err = e.clientset.CoreV1().Pods(namespace).Create(&pod)
+	if err != nil {
+		for _, instance := range instances {
+			err := deployments.SetInstanceState(kv, deploymentID, nodeName, instance, tosca.NodeStateError)
+			if err != nil {
+				return err
+			}
+		}
+		return errors.Wrap(err, "Failed to create pod")
+	}
+
+	for _, instance := range instances {
+		err := deployments.SetInstanceState(kv, deploymentID, nodeName, instance, tosca.NodeStateStarted)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *defaultExecutor) uninstallNode(ctx context.Context, kv *api.KV, cfg config.Configuration, deploymentID, nodeName string, instances []string) error {
+	for _, instance := range instances {
+		err := deployments.SetInstanceState(kv, deploymentID, nodeName, instance, tosca.NodeStateDeleting)
+		if err != nil {
+			return err
+		}
+	}
+	err := e.initClientSet(cfg)
+	if err != nil {
+		return err
+	}
+
+	namespace, err := getNamespace(kv,deploymentID, nodeName)
+	if err != nil {
+		return err
+	}
+
+	err = e.clientset.CoreV1().Pods(strings.ToLower(namespace)).Delete(strings.ToLower(cfg.ResourcesPrefix + nodeName), &metav1.DeleteOptions{})
+	if err != nil {
+		return err
+	}
+	for _, instance := range instances {
+		err := deployments.SetInstanceState(kv, deploymentID, nodeName, instance, tosca.NodeStateDeleted)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getNamespace(kv *api.KV, deploymentID, nodeName string) (string, error) {
+	found, namespace, err := deployments.GetNodeProperty(kv, deploymentID, nodeName, "namespace")
+	if err != nil {
+		return "", err
+	}
+	if !found || namespace == "" {
+		return deployments.GetDeploymentTemplateName(kv, deploymentID)
+	}
+	return namespace, nil
+}
+
+
+
+func (e *defaultExecutor) initClientSet(cfg config.Configuration) error {
 	var clientset *kubernetes.Clientset
 	conf, err := clientcmd.BuildConfigFromFlags(cfg.KubeMasterIp, "")
 	if err != nil {
@@ -61,37 +161,5 @@ func (e *defaultExecutor) installNode(ctx context.Context, kv *api.KV, cfg confi
 	}
 
 	e.clientset = clientset
-	generator := NewGenerator(kv, cfg)
-
-	namespace, err := deployments.GetDeploymentTemplateName(kv, deploymentID)
-	if err != nil {
-		return err
-	}
-
-	namespace = strings.ToLower(namespace)
-	err = generator.CreateNamespaceIfMissing(deploymentID, namespace, clientset)
-	if err != nil {
-		return err
-	}
-
-	pod, err := generator.GeneratePod(deploymentID, nodeName)
-	if err != nil {
-		return err
-	}
-
-	_, err = clientset.CoreV1().Pods(namespace).Create(&pod)
-	return errors.Wrap(err, "Failed to create pod")
-}
-
-func (e *defaultExecutor) uninstallNode(ctx context.Context, kv *api.KV, cfg config.Configuration, deploymentID, nodeName string) error {
-	found, namespace, err := deployments.GetNodeProperty(kv, deploymentID, nodeName, "namespace")
-	if err != nil {
-		return err
-	}
-	if !found || namespace == "" {
-		namespace = "default"
-	}
-
-	err = e.clientset.CoreV1().Pods(namespace).Delete(strings.ToLower(nodeName), &metav1.DeleteOptions{})
-	return err
+	return nil
 }
