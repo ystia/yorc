04086ecea874e9f9be7181ff7f52933aa095cbfc
Albertin Loic <loic.albertin@atos.net>
2017-05-17 15:02:22 +0200

    Fix typo

--- prov/terraform/openstack/init.go
+++ prov/terraform/openstack/init.go
@@ -5,5 +5,5 @@ import "novaforge.bull.com/starlings-janus/janus/prov/terraform"
 
 func init() {
 	reg := registry.GetRegistry()
-	reg.RegisterDelegates([]string{`janus\.nodes.openstack\..*`}, terraform.NewExecutor(&osGenerator{}), registry.BuiltinOrigin)
+	reg.RegisterDelegates([]string{`janus\.nodes\.openstack\..*`}, terraform.NewExecutor(&osGenerator{}), registry.BuiltinOrigin)
 }
