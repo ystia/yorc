fc4cdec601d766730084925b1fbf4465d7deba60
Albertin Loic <loic.albertin@atos.net>
2017-05-23 16:23:32 +0200

    Do not panic in Context cancellation over RPC for plugin clients otherwise it will kill the Janus process which is bad.

--- plugin/context.go
+++ plugin/context.go
@@ -4,15 +4,31 @@ import (
 	"context"
 	"net/rpc"
 
+	"time"
+
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/pkg/errors"
 	"novaforge.bull.com/starlings-janus/janus/log"
 )
 
 func clientMonitorContextCancellation(ctx context.Context, closeChan chan struct{}, id uint32, broker *plugin.MuxBroker) {
+	var dialFailed bool
+RETRY:
 	conn, err := broker.Dial(id)
 	if err != nil {
-		log.Panicf("Failed to dial plugin %+v", errors.WithStack(err))
+		if !dialFailed {
+			log.Printf("[ERROR] Failed to dial plugin %+v \nLet's retry until we succeed or the execution terminate. This that cancellation may not work.", errors.WithStack(err))
+		}
+		dialFailed = true
+		select {
+		case <-closeChan:
+			return
+		case <-time.After(500 * time.Millisecond):
+			goto RETRY
+		}
+	}
+	if dialFailed {
+		log.Printf("Plugin remote context cancellation dial finnally succeeded after failures. Things are back to normal.")
 	}
 	client := rpc.NewClient(conn)
 	defer client.Close()
