7f58a54a0063221120a876976457b4b0e347290c
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-08-01 15:11:14 +0200

    add unit test

--- prov/kubernetes/generator_test.go
+++ prov/kubernetes/generator_test.go
@@ -18,6 +18,7 @@ func TestGenerator(t *testing.T) {
 	t.Run("kubernetes", func(t *testing.T) {
 		t.Run("newGenerator", createSecretRepoTest)
 		t.Run("newGeneratorDeployment", generateDeploymentTest)
+		t.Run("newGeneratorLimitResources", generateLimitResourcesTest)
 	})
 }
 
@@ -74,4 +75,52 @@ func generateDeploymentTest(t *testing.T) {
 	_, _, err = myGenerator.GenerateDeployment(deploymentID, nodeName, operation, nodeTypeName, "test", nil, 1)
 
 	assert.Nil(t, err)
+}
+
+
+func generateLimitResourcesTest(t *testing.T) {
+	t.Parallel()
+
+	resourceList, err := generateLimitsRessources("", "")
+	require.Nil(t, err)
+	require.Nil(t, resourceList)
+
+
+	resourceList, err = generateLimitsRessources("", "1M")
+	require.Nil(t, err)
+	require.NotNil(t, resourceList)
+	i, _ := resourceList.Cpu().AsInt64()
+	require.Equal(t, i, int64(0))
+
+	resourceList, err = generateLimitsRessources("1M", "")
+	require.Nil(t, err)
+	require.NotNil(t, resourceList)
+	i, _ = resourceList.Memory().AsInt64()
+	require.Equal(t, i, int64(0))
+
+	resourceList, err = generateLimitsRessources("qsd", "1M")
+	require.Nil(t, resourceList)
+	require.Error(t, err)
+
+	resourceList, err = generateLimitsRessources("1M", "qsd")
+	require.Nil(t, resourceList)
+	require.Error(t, err)
+
+	resourceList, err = generateLimitsRessources("2M", "1M")
+	require.Nil(t, err)
+	require.NotNil(t, resourceList.Cpu())
+	i, _ = resourceList.Cpu().AsInt64()
+	require.Equal(t, i, int64(2000000))
+	require.NotNil(t, resourceList.Memory())
+	i, _ = resourceList.Memory().AsInt64()
+	require.Equal(t, i, int64(1000000))
+
+	resourceList, err = generateLimitsRessources("0", "0")
+	require.Nil(t, err)
+	require.NotNil(t, resourceList.Cpu())
+	i, _ = resourceList.Cpu().AsInt64()
+	require.Equal(t, i, int64(0))
+	require.NotNil(t, resourceList.Memory())
+	i, _ = resourceList.Memory().AsInt64()
+	require.Equal(t, i, int64(0))
 }
