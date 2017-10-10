b4c71f13de22302294c7972c447ae595d26f445d
stephane benoist <stephane.benoist@atos.net>
2017-10-10 11:32:15 +0200

    bugfix/BRBDCF-681-janus-plugins-can-t-send-errors : extend RPC Error handling for config and operation - Add Unit tests

--- plugin/operation_test.go
+++ plugin/operation_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	plugin "github.com/hashicorp/go-plugin"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"novaforge.bull.com/starlings-janus/janus/config"
 	"novaforge.bull.com/starlings-janus/janus/prov"
@@ -35,6 +36,9 @@ func (m *mockOperationExecutor) ExecOperation(ctx context.Context, conf config.C
 	}()
 	if m.deploymentID == "TestCancel" {
 		<-m.ctx.Done()
+	}
+	if m.deploymentID == "TestFailure" {
+		return NewRPCError(errors.New("a failure occurred during plugin exec operation"))
 	}
 	return nil
 }
@@ -71,6 +75,33 @@ func TestOperationExecutorExecOperation(t *testing.T) {
 	require.Equal(t, "TestDepID", mock.deploymentID)
 	require.Equal(t, "TestNodeName", mock.nodeName)
 	require.Equal(t, op, mock.operation)
+}
+
+func TestOperationExecutorExecOperationWithFailure(t *testing.T) {
+	t.Parallel()
+	mock := new(mockOperationExecutor)
+	client, _ := plugin.TestPluginRPCConn(t, map[string]plugin.Plugin{
+		OperationPluginName: &OperationPlugin{F: func() prov.OperationExecutor {
+			return mock
+		}},
+	})
+	defer client.Close()
+
+	raw, err := client.Dispense(OperationPluginName)
+	require.Nil(t, err)
+
+	plugin := raw.(prov.OperationExecutor)
+	op := prov.Operation{
+		Name: "myOps",
+		ImplementationArtifact: "tosca.artifacts.Implementation.Bash",
+		RelOp: prov.RelationshipOperation{
+			IsRelationshipOperation: true,
+			RequirementIndex:        "1",
+			TargetNodeName:          "AnotherNode",
+		},
+	}
+	err = plugin.ExecOperation(context.Background(), config.Configuration{ConsulAddress: "test", ConsulDatacenter: "testdc"}, "TestTaskID", "TestFailure", "TestNodeName", op)
+	require.Error(t, err, "An error was expected during executing plugin operation")
 }
 
 func TestOperationExecutorExecOperationWithCancel(t *testing.T) {
