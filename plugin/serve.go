bb8c0fd8efbb325f62206638ca651aa15af738c6
Laurent Ganne <ganne.laurent@gmail.com>
2018-03-06 12:34:46 +0000

    plugin version update

--- plugin/serve.go
+++ plugin/serve.go
@@ -45,7 +45,7 @@ const (
 // This prevents users from executing bad plugins or executing a plugin
 // directory. It is a UX feature, not a security feature.
 var HandshakeConfig = plugin.HandshakeConfig{
-	ProtocolVersion:  2,
+	ProtocolVersion:  3,
 	MagicCookieKey:   "YORC_PLUG_API",
 	MagicCookieValue: "a3292e718f7c96578aae47e92b7475394e72e6da3de3455554462ba15dde56d1b3187ad0e5f809f50767e0d10ca6944fdf4c6c412380d3aa083b9e8951f7101e",
 }
