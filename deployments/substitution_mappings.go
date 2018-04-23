12111aa3fb924f5abc62ad2d13b549b37cc0d5e7
Laurent Ganne <ganne.laurent@gmail.com>
2018-04-23 11:35:20 +0000

    Managing substitutions of capability attributes

--- deployments/substitution_mappings.go
+++ deployments/substitution_mappings.go
@@ -16,25 +16,31 @@ package deployments
 
 import (
 	"context"
+	"fmt"
 	"path"
 	"strings"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/pkg/errors"
 	"github.com/ystia/yorc/helper/consulutil"
+	"github.com/ystia/yorc/log"
 	"github.com/ystia/yorc/tosca"
 )
 
 const (
-	// DirectiveSubstitutable is a directive to the Orchestrator that a node
-	// type is substitutable, ie. if this node type is a reference to another
-	// topology template providing substitution mappings
-	DirectiveSubstitutable = "substitutable"
+	// directiveSubstitutable is a directive to the Orchestrator that a node
+	// type is substitutable, ie. this node type is either abstract or a
+	// reference to another topology template providing substitution mappings
+	directiveSubstitutable = "substitutable"
+
+	// Name of a fake instance for a substitutable node
+	// Using an integer value as this is expected by the Orchestrator plugin
+	substitutableNodeInstance = "-1"
 )
 
-// IsSubstitutableNode returns true if a node contains an Orchestrator directive
+// isSubstitutableNode returns true if a node contains an Orchestrator directive
 // that it is substitutable
-func IsSubstitutableNode(kv *api.KV, deploymentID, nodeName string) (bool, error) {
+func isSubstitutableNode(kv *api.KV, deploymentID, nodeName string) (bool, error) {
 	kvp, _, err := kv.Get(path.Join(consulutil.DeploymentKVPrefix, deploymentID, "topology/nodes", nodeName, "directives"), nil)
 	if err != nil {
 		return false, errors.Wrapf(err, "Can't get directives for node %q", nodeName)
@@ -44,7 +50,7 @@ func IsSubstitutableNode(kv *api.KV, deploymentID, nodeName string) (bool, error
 	if kvp != nil && kvp.Value != nil {
 		values := strings.Split(string(kvp.Value), ",")
 		for _, value := range values {
-			if value == DirectiveSubstitutable {
+			if value == directiveSubstitutable {
 				substitutable = true
 				break
 			}
@@ -153,6 +159,11 @@ func storeInterfaceMappings(
 	}
 }
 
+func getDeploymentSubstitutionMapping(kv *api.KV, deploymentID string) (tosca.SubstitutionMapping, error) {
+	return getSubstitutionMappingFromStore(kv,
+		path.Join(consulutil.DeploymentKVPrefix, deploymentID, "topology"))
+}
+
 func getSubstitutionMappingFromStore(kv *api.KV, prefix string) (tosca.SubstitutionMapping, error) {
 	substitutionPrefix := path.Join(prefix, "substitution_mappings")
 	var substitutionMapping tosca.SubstitutionMapping
@@ -163,9 +174,8 @@ func getSubstitutionMappingFromStore(kv *api.KV, prefix string) (tosca.Substitut
 			errors.Wrapf(err, "Can't get node type for substitution at %q", substitutionPrefix)
 	}
 	if kvp == nil || len(kvp.Value) == 0 {
-		return substitutionMapping,
-			errors.Errorf("Missing mandatory value for %q for substitution at %q",
-				"node_type", substitutionPrefix)
+		// No mapping defined
+		return substitutionMapping, nil
 	}
 	substitutionMapping.NodeType = string(kvp.Value)
 	substitutionMapping.Capabilities, err = getCapReqMappingFromStore(
@@ -179,6 +189,220 @@ func getSubstitutionMappingFromStore(kv *api.KV, prefix string) (tosca.Substitut
 }
 
 func getCapReqMappingFromStore(kv *api.KV, prefix string) (map[string]tosca.CapReqMapping, error) {
-	var result map[string]tosca.CapReqMapping
-	return result, nil
+
+	capabilityPaths, _, err := kv.Keys(prefix+"/", "/", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	capabilities := make(map[string]tosca.CapReqMapping)
+
+	for _, capPath := range capabilityPaths {
+
+		capName := path.Base(capPath)
+
+		kvp, _, err := kv.Get(path.Join(capPath, "mapping"), nil)
+		if err != nil {
+			return capabilities,
+				errors.Wrapf(err, "Can't get mapping for capability at %q", capPath)
+		}
+		var capMapping tosca.CapReqMapping
+		if kvp != nil {
+			capMapping.Mapping = strings.Split(string(kvp.Value), ",")
+		}
+		/*
+			propertiesPrefix := path.Join(capPath, "properties")
+			propertyPaths, _, err := kv.Keys(propertiesPrefix+"/", "/", nil)
+			if err != nil {
+				return nil, err
+			}
+		*/
+		properties := make(map[string]*tosca.ValueAssignment)
+
+		// TODO: continue the parsing
+		capMapping.Properties = properties
+
+		capabilities[capName] = capMapping
+	}
+
+	return capabilities, nil
+}
+
+// storeSubstitutionMappingAttributeNamesInSet gets capability attributes for capabilities
+// exposed in the deployment through substitution mappings, and stores these
+// capability attributes as node attributes prefixed by capabilities.<capability name>
+// as expected by Alien4Cloud until it implements the mapping of capability attributes
+func storeSubstitutionMappingAttributeNamesInSet(kv *api.KV, deploymentID, nodeName string, set map[string]struct{}) error {
+
+	substMapping, err := getDeploymentSubstitutionMapping(kv, deploymentID)
+	if err != nil {
+		return err
+	}
+
+	capabilityToAttrNames := make(map[string][]string)
+	exploreParents := true
+	// Get the capabilities exposed for this node
+	for _, capMapping := range substMapping.Capabilities {
+
+		capability := capMapping.Mapping[1]
+		var attributeNames []string
+
+		if capMapping.Mapping[0] == nodeName {
+			if capMapping.Attributes != nil {
+				attributeNames := make([]string, len(capMapping.Attributes))
+				i := 0
+				for name := range capMapping.Attributes {
+					attributeNames[i] = name
+					i++
+				}
+			} else {
+				// Expose all attributes
+				attributeNames, err = GetNodeCapabilityAttributeNames(
+					kv, deploymentID, nodeName, capability, exploreParents)
+				if err != nil {
+					return err
+				}
+			}
+
+			capabilityToAttrNames[capability] = attributeNames
+		}
+	}
+
+	// See org.alien4cloud.alm.service.ManagedServiceResourceEventService,
+	// method mapCapabilityRequirementAttributes. This method expects
+	// to get capability attributes as instances attributes with the prefix
+	// capabilities.<capability name>.<attribute name>
+	capabilityFormat := "capabilities.%s.%s"
+	for capability, names := range capabilityToAttrNames {
+		for _, attr := range names {
+			set[fmt.Sprintf(capabilityFormat, capability, attr)] = struct{}{}
+		}
+	}
+
+	return nil
+}
+
+func isSubstitutionMappingAttribute(attributeName string) bool {
+	items := strings.Split(attributeName, ".")
+	return len(items) == 3 && items[0] == "capabilities"
+}
+
+// getSubstitutionMappingAttribute retrieves the given attribute for a capability
+//
+// It returns true if a value is found false otherwise as first return parameter.
+// If the attribute is a substitution mapping capability attribute as provided
+// by Alien4Cloud, using the format capabilities.<capability name>.<attributr name>,
+// this function returns the the vqlue of the corresponding instance capability
+// attribute
+func getSubstitutionMappingAttribute(kv *api.KV, deploymentID, nodeName, instanceName, attributeName string, nestedKeys ...string) (bool, string, error) {
+
+	if !isSubstitutionMappingAttribute(attributeName) {
+		return false, "", nil
+	}
+
+	log.Debugf("Attempting to substitute attribute %s in %s %s %s", attributeName, deploymentID, nodeName, instanceName)
+
+	items := strings.Split(attributeName, ".")
+	capabilityName := items[1]
+	capAttrName := items[2]
+
+	// Check this capability attribute is really exposed before returning its
+	// value
+	attributesSet := make(map[string]struct{})
+	err := storeSubstitutionMappingAttributeNamesInSet(kv, deploymentID, nodeName, attributesSet)
+	if err != nil {
+		return false, "", err
+	}
+
+	if _, ok := attributesSet[attributeName]; ok {
+		// This attribute is exposed, returning its value
+		log.Debugf("Substituting attribute %s by its instance capability attribute in %s %s %s %s", attributeName, deploymentID, nodeName, instanceName)
+		return GetInstanceCapabilityAttribute(
+			kv, deploymentID, nodeName, instanceName, capabilityName, capAttrName, nestedKeys...)
+	}
+
+	return false, "", nil
+}
+
+// getSubstitutionNodeInstancesIds returns for a substitutable node, a fake
+// instance ID, all necessary infos being stored at the node level.
+func getSubstitutionNodeInstancesIds(kv *api.KV, deploymentID, nodeName string) ([]string, error) {
+
+	var names []string
+	substitutable, err := isSubstitutableNode(kv, deploymentID, nodeName)
+	if err == nil && substitutable {
+		names = []string{substitutableNodeInstance}
+	}
+	return names, err
+}
+
+func isSubstitutionNodeInstance(nodeInstance string) bool {
+	return nodeInstance == substitutableNodeInstance
+}
+
+// getSubstitutableNodeType returns the node type of a substitutable node.
+// There are 2 cases :
+// - either this node a reference to an external service, and the node type
+//   here is a real node type (abstract)
+// - or this is a reference to a managed service, and the node type here
+//   does not exist, it is the name of a template whose import contains the
+//    real node type
+func getSubstitutableNodeType(kv *api.KV, deploymentID, nodeName, nodeType string) (string, error) {
+	_, err := GetParentType(kv, deploymentID, nodeType)
+	if err == nil {
+		// Rference to an external service, this node type is a eal abstract
+		// node type
+		return nodeType, nil
+	} else if !IsTypeMissingError(err) {
+		// Unexpected error
+		return nodeType, err
+	}
+
+	// The node type does not exist. This is the  reference to an application
+	// from another deployment.
+	// The real node type has to be found in subsitution mappings of an imported
+	// file whose metadata template name is the nodeType here.
+	importsPath := path.Join(consulutil.DeploymentKVPrefix, deploymentID, "topology/imports")
+	imports, _, err := kv.Keys(importsPath+"/", "/", nil)
+	if err != nil {
+		return "", errors.Wrap(err, "Consul communication error")
+	}
+
+	var importTemplatePath string
+	for _, importPath := range imports {
+		kvp, _, err := kv.Get(path.Join(importPath, "metadata/template_name"), nil)
+		if err == nil && kvp != nil && len(kvp.Value) != 0 && string(kvp.Value) == nodeType {
+			// Found the import
+			importTemplatePath = importPath
+			break
+		}
+	}
+
+	if importTemplatePath == "" {
+		// No such template found
+		return nodeType, nil
+	}
+
+	// Check subsitution mappings in this import
+	mappings, err := getSubstitutionMappingFromStore(kv, importTemplatePath)
+	if err == nil && mappings.NodeType != "" {
+		log.Debugf("Substituting type %s by type %s for %s %s", nodeType, mappings.NodeType, deploymentID, nodeName)
+		return mappings.NodeType, err
+	}
+	// Found no substitution type
+	return nodeType, nil
+
+}
+
+// getSubstitutionInstanceAttribute returns the value of generic attributes
+// associated to any instance, here a fake instance for a substitutable node
+func getSubstitutionInstanceAttribute(deploymentID, nodeName, instanceName, attributeName string) (bool, string) {
+	switch attributeName {
+	case "tosca_name":
+		return true, nodeName
+	case "tosca_id":
+		return true, nodeName + "-" + instanceName
+	default:
+		return false, ""
+	}
 }
