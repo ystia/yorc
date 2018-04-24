a856f4b1053e5424d9856be6e491f2429227f8fd
ROSSO, LOIC <loic.rosso@atos.net>
2018-04-24 16:22:22 +0200

    first test works with defaults certs

--- rest/tls_test.go
+++ rest/tls_test.go
@@ -15,6 +15,11 @@
 package rest
 
 import (
+	"crypto/tls"
+	_ "crypto/x509"
+	_"crypto/tls"
+	"fmt"
+	
 	"net/http"
 	"github.com/stretchr/testify/require"
 	"github.com/ystia/yorc/config"
@@ -37,12 +42,13 @@ import (
 		t.Run("testSSLEnabledNoVerify", func(t *testing.T) {
 			testSSLEnabledNoVerify(t, client, srv)
 		})
+		/*
 		t.Run("testSSLEnabledVerifyUnsignedCerts", func(t *testing.T) {
 			testSSLEnabledVerifyUnsignedCerts(t, client, srv)
 		})
 		t.Run("testSSLEnabledVerifySignedCerts", func(t *testing.T) {
 			testSSLEnabledVerifySignedCerts(t, client, srv)
-		})
+		}) */
 	}
 
 	func testSSLEnabledNoCerts(t *testing.T, client *api.Client, srv *testutil.TestServer) {
@@ -53,7 +59,7 @@ import (
 			SSLVerify: false,
 		}
 
-		resp, err := newTestHTTPSRouter(cfg, client, nil)
+		resp, err := newTestHTTPSRouter(cfg, client, nil, nil)
 		require.NotNil(t, err, "unexpected nil error : SSL without certs should raise error")
 		require.Nil(t, resp, "unexpected Not nil response")
 	}
@@ -68,29 +74,85 @@ import (
 			KeyFile: "testdata/server-key.pem",
 		}
 
-	 	resp, err := newTestHTTPSRouter(cfg, client, nil)
+	 	resp, err := newTestHTTPSRouter(cfg, client, nil, nil)
 		require.NotNil(t, err, "unexpected nil error : SSL verify without CA should raise error")
 		require.Nil(t, resp, "unexpected Not nil response")
 	}
 
 	func testSSLEnabledNoVerify(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+		log.SetDebug(true)
 		t.Parallel()
-
-		req := httptest.NewRequest("GET", "/deployments", nil)
-		req.Header.Add("Accept", "application/json")
+		/*		
 		cfg := config.Configuration{
-			SSLEnabled: true,
 			SSLVerify: false,
 			CertFile: "testdata/server-cert.pem",
 			KeyFile: "testdata/server-key.pem",
 		}
+		*/
+		log.Debugln("-------ServersTLScreation------")
+		ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Hello, client")
+		}))
+		//ts.Listener, _ = wrapListenerTLS(ts.Listener, cfg)
+		ts.StartTLS()
+		defer ts.Close()
+/*
+		cert, err := tls.LoadX509KeyPair("testdata/client-cert.pem", "testdata/client-key.pem")
+		if err != nil {
+			log.Fatal( "Failed to load TLS certificates")
+		}
+		
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates: []tls.Certificate{cert},
+				InsecureSkipVerify: true,
+			},
+		}hclient := ts.Client()
+		hclient.Transport = tr
+		
+		cert, err := x509.ParseCertificate(ts.TLS.Certificates[0].Certificate[0])
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		resp, err := newTestHTTPSRouter(cfg, client, req)
-		require.Nil(t, err, "unexpected error creating router")
-		_, err = ioutil.ReadAll(resp.Body)
+		certpool := x509.NewCertPool()
+		certpool.AddCert(cert)
+*/
+/*
+		hclient := http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					//RootCAs: certpool,
+					InsecureSkipVerify: true,
+				},
+			},
+		}
+		*/
+		hclient := http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		}
+		resp, err := hclient.Get(ts.URL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		//resp, err := newTestHTTPSRouter(cfg, client, hclient, req)
+		//require.Nil(t, err, "unexpected error creating router")
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Debugln("----------Body-------")
+		log.Debugln(string(body))
+		/*
 		require.Nil(t, err, "unexpected error reading body response")
 		require.NotNil(t, resp, "unexpected nil response")
-		require.Equal(t, http.StatusNoContent, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest)
+		require.Equal(t, http.StatusOK, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest) */
 	}
 
 	func testSSLEnabledVerifyUnsignedCerts(t *testing.T, client *api.Client, srv *testutil.TestServer) {
@@ -106,9 +168,11 @@ import (
 			CAFile: "testdata/ca-cert.pem",
 		}
 
-		resp, err := newTestHTTPSRouter(cfg, client, req)
+		resp, err := newTestHTTPSRouter(cfg, client, nil, req)
 		require.Nil(t, err, "unexpected error creating router")
-		_, err = ioutil.ReadAll(resp.Body)
+		body, err := ioutil.ReadAll(resp.Body)
+		log.Debugln("----------Body-------")
+		log.Debugln(string(body))
 		require.Nil(t, err, "unexpected error reading body response")
 		require.NotNil(t, resp, "unexpected nil response")
 		require.Equal(t, http.StatusNoContent, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest)
@@ -127,9 +191,11 @@ import (
 			CAFile: "testdata/ca-cert.pem",
 		}
 
-		resp, err := newTestHTTPSRouter(cfg, client, req)
+		resp, err := newTestHTTPSRouter(cfg, client, nil, req)
 		require.Nil(t, err, "unexpected error creating router")
-		_, err = ioutil.ReadAll(resp.Body)
+		body, err := ioutil.ReadAll(resp.Body)
+		log.Debugln("----------Body-------")
+		log.Debugln(string(body))
 		require.Nil(t, err, "unexpected error reading body response")
 		require.NotNil(t, resp, "unexpected nil response")
 		require.Equal(t, http.StatusNoContent, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest)
