10fccb303df165ada5238591f7e9d5d0a05e823d
Albertin Loic <loic.albertin@atos.net>
2018-05-09 16:36:21 +0200

    Update consul version in TF install

--- testdata/infra/scripts/install_consul.sh
+++ testdata/infra/scripts/install_consul.sh
@@ -16,7 +16,7 @@
 set -ex
 
 sudo yum install -q -y wget zip unzip
-cd /tmp && wget -q https://releases.hashicorp.com/consul/0.8.4/consul_0.8.4_linux_amd64.zip && sudo unzip /tmp/consul_0.8.4_linux_amd64.zip -d /usr/local/bin
+cd /tmp && wget -q https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip && sudo unzip /tmp/consul_1.0.6_linux_amd64.zip -d /usr/local/bin
 sudo mv /tmp/consul.service /etc/systemd/system/consul.service
 sudo chown root:root /etc/systemd/system/consul.service
 sudo mkdir -p /etc/consul.d
