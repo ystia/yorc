6eeec6e1c399f2a0676542f829e0127098a1a4c8
Albertin Loic <loic.albertin@atos.net>
2019-01-10 17:18:41 +0100

    Artifactory integration - Fix build name - collect git info instead of env vars

--- build/deploy_artifactory.sh
+++ build/deploy_artifactory.sh
@@ -9,7 +9,7 @@ if [[ "${TRAVIS}" != "true" ]] ; then
 fi
 
 if [[ -n "${TRAVIS_TAG}" ]] ; then
-    deploy_path="yorc-bin-release-local/ystia/yorc/dist/${TRAVIS_TAG}/{1}"
+    deploy_path="yorc-engine-product-ystia-dist/ystia/yorc/dist/${TRAVIS_TAG}/{1}"
 elif [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     deploy_path="yorc-bin-dev-local/ystia/yorc/dist/PR-${TRAVIS_PULL_REQUEST}/{1}"
 else
@@ -18,8 +18,12 @@ fi
 
 curl -fL https://getcli.jfrog.io | sh
 
+build_name="yorc-travis-ci"
+
 ./jfrog rt c --apikey="${ARTIFACTORY_API_KEY}" --url=https://ystia.jfrog.io/ystia ystia
-./jfrog rt u --build-name=my-build-name --build-number="${TRAVIS_BUILD_NUMBER}" --regexp "dist/(yorc-.*.tgz)" "${deploy_path}"
-./jfrog rt u --build-name=my-build-name --build-number="${TRAVIS_BUILD_NUMBER}" --regexp "dist/(yorc-server.*-distrib.zip)" "${deploy_path}"
-./jfrog rt bce my-build-name "${TRAVIS_BUILD_NUMBER}"
-./jfrog rt bp my-build-name "${TRAVIS_BUILD_NUMBER}"
+./jfrog rt u --build-name="${build_name}" --build-number="${TRAVIS_BUILD_NUMBER}" --regexp "dist/(yorc-.*.tgz)" "${deploy_path}"
+./jfrog rt u --build-name="${build_name}" --build-number="${TRAVIS_BUILD_NUMBER}" --regexp "dist/(yorc-server.*-distrib.zip)" "${deploy_path}"
+# Do not publish environment variables as it may expose some secrets
+#./jfrog rt bce "${build_name}" "${TRAVIS_BUILD_NUMBER}"
+./jfrog rt bag "${build_name}" "${TRAVIS_BUILD_NUMBER}"
+./jfrog rt bp "${build_name}" "${TRAVIS_BUILD_NUMBER}"
