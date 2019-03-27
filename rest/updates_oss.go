4f038c71d30168ab904663ce39767c991cd058e4
Laurent Ganne <ganne.laurent@gmail.com>
2019-03-27 09:33:03 +0000

    Updated error message

--- rest/updates_oss.go
+++ rest/updates_oss.go
@@ -25,7 +25,7 @@ import (
 
 // updateDeployment updates a deployment
 func (s *Server) updateDeployment(w http.ResponseWriter, r *http.Request, id string) {
-	msg := fmt.Sprintf("Deployment with id %q already exists - Update not supported by this Orchestrator version", id)
+	msg := fmt.Sprintf("Deployment with id %q already exists - Update supported only in premium versions", id)
 	log.Debugf("[ERROR]: %s", msg)
 	writeError(w, r, newConflictRequest(msg))
 }
