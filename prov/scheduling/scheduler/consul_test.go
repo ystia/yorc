6a5d8f51e5f78d88075d8fbfe0753a0b759e6ee3
benoist-s <stephane.benoist@atos.net>
2019-06-17 09:52:12 +0200

    Add new test

--- prov/scheduling/scheduler/consul_test.go
+++ prov/scheduling/scheduler/consul_test.go
@@ -52,6 +52,9 @@ func TestRunConsulSchedulingPackageTests(t *testing.T) {
 		t.Run("testProceedScheduledActionWithFirstActionStillRunning", func(t *testing.T) {
 			testProceedScheduledActionWithFirstActionStillRunning(t, client)
 		})
+		t.Run("testProceedScheduledActionWithBadStatusError", func(t *testing.T) {
+			testProceedScheduledActionWithBadStatusError(t, client)
+		})
 		t.Run("testUnregisterAction", func(t *testing.T) {
 			testUnregisterAction(t, client)
 		})
--- prov/scheduling/scheduler/scheduler_test.go
+++ prov/scheduling/scheduler/scheduler_test.go
@@ -172,7 +172,7 @@ func testProceedScheduledActionWithFirstActionStillRunning(t *testing.T, client 
 				continue
 			}
 
-			// Delete the related task to reschedule another one
+			// Set the task status to RUNNING in order to not reschedule another task
 			if kvp != nil && len(kvp.Value) > 0 {
 				taskID := string(kvp.Value)
 				p := &api.KVPair{Key: path.Join(consulutil.TasksPrefix, taskID, "status"), Value: []byte(strconv.Itoa(int(tasks.TaskStatusRUNNING)))}
@@ -225,6 +225,99 @@ func testProceedScheduledActionWithFirstActionStillRunning(t *testing.T, client 
 		case <-ticker.C:
 			ind++
 			// as the task is still running, no other task is created
+			check(1, checkCpt)
+			if ind == 3 {
+				ticker.Stop()
+				return
+			}
+		}
+	}
+
+	require.Equal(t, checkCpt, 5, "unexpected number of checks done")
+}
+
+func testProceedScheduledActionWithBadStatusError(t *testing.T, client *api.Client) {
+	t.Parallel()
+	deploymentID := "dep-" + t.Name()
+	ti := 1 * time.Second
+	actionType := "test-action"
+	action := &prov.Action{ActionType: actionType, Data: map[string]string{"key1": "val1", "key2": "val2", "key3": "val3"}}
+	id, err := scheduling.RegisterAction(client, deploymentID, ti, action)
+	require.Nil(t, err, "Unexpected error while registering action")
+	require.NotEmpty(t, id, "id is not expected to be empty")
+
+	closeCh := make(chan struct{})
+	defer close(closeCh)
+	go func() {
+		var latestIndex uint64
+		for {
+			select {
+			case <-closeCh:
+				return
+			default:
+			}
+			kvp, meta, err := client.KV().Get(path.Join(consulutil.SchedulingKVPrefix, "actions", id, "latestTaskID"), &api.QueryOptions{WaitIndex: latestIndex})
+			if err != nil {
+				t.Logf("%v", err)
+				continue
+			}
+			if latestIndex == meta.LastIndex {
+				continue
+			}
+
+			// Set the task status to RUNNING in order to not reschedule another task
+			if kvp != nil && len(kvp.Value) > 0 {
+				taskID := string(kvp.Value)
+				p := &api.KVPair{Key: path.Join(consulutil.TasksPrefix, taskID, "status"), Value: []byte("BAD")}
+				client.KV().Put(p, nil)
+			}
+
+		}
+	}()
+
+	var check = func(index, cpt int) {
+		cpt++
+		// Check related tasks have been created
+		keys, _, err := client.KV().Keys(consulutil.TasksPrefix+"/", "/", nil)
+		require.Nil(t, err, "Unexpected error while checking actions tasks")
+		depTask := 0
+		for _, key := range keys {
+			kvp, _, err := client.KV().Get(key+"targetId", nil)
+			if kvp != nil && string(kvp.Value) == deploymentID {
+				depTask++
+				kvp, _, err = client.KV().Get(key+"data/actionType", nil)
+				require.Nil(t, err, "Unexpected error while getting action type")
+				require.NotNil(t, kvp, "kvp is nil for action type")
+				require.Equal(t, string(kvp.Value), actionType)
+
+				kvp, _, err = client.KV().Get(key+"data/key1", nil)
+				require.Nil(t, err, "Unexpected error while getting key1")
+				require.NotNil(t, kvp, "kvp is nil for key1")
+				require.Equal(t, string(kvp.Value), "val1")
+
+				kvp, _, err = client.KV().Get(key+"data/key2", nil)
+				require.Nil(t, err, "Unexpected error while getting key3")
+				require.NotNil(t, kvp, "kvp is nil for key2")
+				require.Equal(t, string(kvp.Value), "val2")
+
+				kvp, _, err = client.KV().Get(key+"data/key3", nil)
+				require.Nil(t, err, "Unexpected error while getting key3")
+				require.NotNil(t, kvp, "kvp is nil for key3")
+				require.Equal(t, string(kvp.Value), "val3")
+			}
+		}
+		require.Equal(t, index, depTask, "Unexpected nb of tasks")
+	}
+
+	ind := 0
+	checkCpt := 0
+	ticker := time.NewTicker(ti)
+	time.Sleep(2 * time.Second)
+	for i := 0; i <= 2; i++ {
+		select {
+		case <-ticker.C:
+			ind++
+			// as the proceed returns an error, the scheduler will stop and only one task will be created
 			check(1, checkCpt)
 			if ind == 3 {
 				ticker.Stop()
