28f97904fb9174f900159303f3a91afaf815ea32
Laurent Ganne <ganne.laurent@gmail.com>
2019-07-16 08:13:24 +0000

    Uncommented test

--- deployments/consul_test.go
+++ deployments/consul_test.go
@@ -103,11 +103,9 @@ func TestRunConsulDeploymentsPackageTests(t *testing.T) {
 		t.Run("testRepositories", func(t *testing.T) {
 			testRepositories(t, kv)
 		})
-		/*
-			t.Run("testPurgedDeployments", func(t *testing.T) {
-				testPurgedDeployments(t, client)
-			})
-		*/
+		t.Run("testPurgedDeployments", func(t *testing.T) {
+			testPurgedDeployments(t, client)
+		})
 	})
 
 	t.Run("CommonsTestsOn_test_topology.yml", func(t *testing.T) {
