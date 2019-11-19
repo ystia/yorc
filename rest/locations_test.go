e99790d6ed5d6934c363b1cacfeb6d009e0e4d59
Adriana Danes <adriana.danes@atos.net>
2019-11-19 11:52:34 +0100

    Add rest tests for location management

--- rest/locations_test.go
+++ rest/locations_test.go
@@ -15,6 +15,7 @@
 package rest
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -24,21 +25,38 @@ import (
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/testutil"
 	"github.com/stretchr/testify/require"
+	"github.com/ystia/yorc/v4/config"
 	"github.com/ystia/yorc/v4/helper/consulutil"
 	"github.com/ystia/yorc/v4/log"
 )
 
 func testLocationsHandlers(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	populateKV(t, srv)
 	t.Run("testListLocations", func(t *testing.T) {
 		testListLocations(t, client, srv)
 	})
 	t.Run("testDeleteLocation", func(t *testing.T) {
 		testDeleteLocation(t, client, srv)
 	})
+	t.Run("testDeleteLocation", func(t *testing.T) {
+		testDeleteInexistentLocation(t, client, srv)
+	})
+	t.Run("testGetLocation", func(t *testing.T) {
+		testGetLocationInfo(t, client, srv)
+	})
+	t.Run("testCreateNoDataLocation", func(t *testing.T) {
+		testCreateNoDataLocation(t, client, srv)
+	})
+	t.Run("testCreateLocation", func(t *testing.T) {
+		testCreateLocation(t, client, srv)
+	})
+	t.Run("testUpdateLocation", func(t *testing.T) {
+		testUpdateLocation(t, client, srv)
+	})
+	cleanUpKV(client)
 }
 
-func testListLocations(t *testing.T, client *api.Client, srv *testutil.TestServer) {
-	log.SetDebug(true)
+func populateKV(t *testing.T, srv *testutil.TestServer) {
 
 	// Populate KV base with some location definitions
 	srv.PopulateKV(t, map[string][]byte{
@@ -46,6 +64,16 @@ func testListLocations(t *testing.T, client *api.Client, srv *testutil.TestServe
 		consulutil.LocationsPrefix + "/loc2": []byte("{\"Name\":\"loc2\",\"Type\":\"t2\",\"Properties\":{\"p12\":\"v12\",\"p22\":\"v22\"}}"),
 		consulutil.LocationsPrefix + "/loc3": []byte("{\"Name\":\"loc3\",\"Type\":\"t3\",\"Properties\":{\"p13\":\"v13\",\"p23\":\"v23\"}}"),
 	})
+}
+
+func cleanUpKV(client *api.Client) {
+	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc1", nil)
+	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc2", nil)
+	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc3", nil)
+}
+
+func testListLocations(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	log.SetDebug(true)
 
 	// Make a request
 	req := httptest.NewRequest("GET", "/locations", nil)
@@ -70,18 +98,9 @@ func testListLocations(t *testing.T, client *api.Client, srv *testutil.TestServe
 	require.Equal(t, "/locations/loc3", collection.Locations[2].Href)
 	require.Equal(t, "location", collection.Locations[2].Rel)
 
-	// Clean-up KV
-	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc1", nil)
-	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc2", nil)
-	client.KV().DeleteTree(consulutil.LocationsPrefix+"/loc3", nil)
 }
 
 func testDeleteLocation(t *testing.T, client *api.Client, srv *testutil.TestServer) {
-	t.Parallel()
-	srv.PopulateKV(t, map[string][]byte{
-		consulutil.LocationsPrefix + "/loc1": []byte("{\"Name\":\"loc1\",\"Type\":\"t1\",\"Properties\":{\"p11\":\"v11\",\"p21\":\"v21\"}}"),
-	})
-
 	req := httptest.NewRequest("DELETE", "/locations/loc1", nil)
 	resp := newTestHTTPRouter(client, req)
 	_, err := ioutil.ReadAll(resp.Body)
@@ -92,12 +111,7 @@ func testDeleteLocation(t *testing.T, client *api.Client, srv *testutil.TestServ
 }
 
 func testDeleteInexistentLocation(t *testing.T, client *api.Client, srv *testutil.TestServer) {
-	t.Parallel()
-	srv.PopulateKV(t, map[string][]byte{
-		consulutil.LocationsPrefix + "/loc1": []byte("{\"Name\":\"loc1\",\"Type\":\"t1\",\"Properties\":{\"p11\":\"v11\",\"p21\":\"v21\"}}"),
-	})
-
-	req := httptest.NewRequest("DELETE", "/locations/loc2", nil)
+	req := httptest.NewRequest("DELETE", "/locations/loc1", nil)
 	resp := newTestHTTPRouter(client, req)
 	_, err := ioutil.ReadAll(resp.Body)
 
@@ -105,4 +119,72 @@ func testDeleteInexistentLocation(t *testing.T, client *api.Client, srv *testuti
 	require.NotNil(t, resp, "unexpected nil response")
 
 	require.Equal(t, http.StatusBadRequest, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest)
+}
+
+func testGetLocationInfo(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	req := httptest.NewRequest("GET", "/locations/loc2", nil)
+	req.Header.Add("Accept", "application/json")
+	resp := newTestHTTPRouter(client, req)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	require.Nil(t, err, "unexpected error reading body response")
+	require.NotNil(t, resp, "unexpected nil response")
+	require.NotNil(t, body, "unexpected nil body")
+	require.Equal(t, http.StatusOK, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusOK)
+}
+
+func testCreateNoDataLocation(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	req := httptest.NewRequest("PUT", "/locations/loc1", nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp := newTestHTTPRouter(client, req)
+	_, err := ioutil.ReadAll(resp.Body)
+
+	require.Nil(t, err, "unexpected error reading body response")
+	require.NotNil(t, resp, "unexpected nil response")
+	require.Equal(t, http.StatusBadRequest, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusBadRequest)
+}
+
+func testCreateLocation(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	locationProps := config.DynamicMap{
+		"p11": "v11",
+		"p12": "v12",
+	}
+	locationReq := LocationRequest{
+		Type:       "t1",
+		Properties: locationProps,
+	}
+	tmp, err := json.Marshal(locationReq)
+	require.Nil(t, err, "unexpected error marshalling data to provide body request")
+	req := httptest.NewRequest("PUT", "/locations/loc1", bytes.NewBuffer([]byte(string(tmp))))
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp := newTestHTTPRouter(client, req)
+	_, err = ioutil.ReadAll(resp.Body)
+
+	require.Nil(t, err, "unexpected error reading body response")
+	require.NotNil(t, resp, "unexpected nil response")
+	require.Equal(t, http.StatusCreated, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusCreated)
+}
+
+func testUpdateLocation(t *testing.T, client *api.Client, srv *testutil.TestServer) {
+	locationProps := config.DynamicMap{
+		"p11": "v11",
+		"p12": "v15",
+	}
+	locationReq := LocationRequest{
+		Type:       "t1",
+		Properties: locationProps,
+	}
+	tmp, err := json.Marshal(locationReq)
+	require.Nil(t, err, "unexpected error marshalling data to provide body request")
+	req := httptest.NewRequest("PATCH", "/locations/loc1", bytes.NewBuffer([]byte(string(tmp))))
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp := newTestHTTPRouter(client, req)
+	_, err = ioutil.ReadAll(resp.Body)
+
+	require.Nil(t, err, "unexpected error reading body response")
+	require.NotNil(t, resp, "unexpected nil response")
+	require.Equal(t, http.StatusOK, resp.StatusCode, "unexpected status code %d instead of %d", resp.StatusCode, http.StatusOK)
 }
