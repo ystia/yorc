07e43e1899522e0788279dacd8c965df3585bb42
hilderic <hilderic.sauret-bourin@atos.net>
2019-12-16 11:21:56 +0100

    Reducing complexity of GenerateTerraformInfra

--- prov/terraform/aws/generator.go
+++ prov/terraform/aws/generator.go
@@ -84,47 +84,14 @@ func (g *awsGenerator) GenerateTerraformInfraForNode(ctx context.Context, cfg co
 	}
 
 	log.Debugf("inspecting node %s", nodeName)
-	nodeType, err := deployments.GetNodeType(ctx, deploymentID, nodeName)
-	if err != nil {
-		return false, nil, nil, nil, err
-	}
 	outputs := make(map[string]string)
 	instances, err := deployments.GetNodeInstancesIds(ctx, deploymentID, nodeName)
 	if err != nil {
 		return false, nil, nil, nil, err
 	}
-
-	for instNb, instanceName := range instances {
-		instanceState, err := deployments.GetInstanceState(ctx, deploymentID, nodeName, instanceName)
-		if err != nil {
-			return false, nil, nil, nil, err
-		}
-		if instanceState == tosca.NodeStateDeleting || instanceState == tosca.NodeStateDeleted {
-			// Do not generate something for this node instance (will be deleted if exists)
-			continue
-		}
-
-		switch nodeType {
-		case "yorc.nodes.aws.Compute":
-			instances, err = deployments.GetNodeInstancesIds(ctx, deploymentID, nodeName)
-			if err != nil {
-				return false, nil, nil, nil, err
-			}
-
-			err = g.generateAWSInstance(ctx, cfg, deploymentID, nodeName, instanceName, &infrastructure, outputs, &cmdEnv)
-			if err != nil {
-				return false, nil, nil, nil, err
-			}
-		case "yorc.nodes.aws.PublicNetwork":
-			// Nothing to do
-		case "yorc.nodes.aws.EBSVolume":
-			err = g.generateEBS(instanceName, instNb)
-			if err != nil {
-				return false, nil, nil, nil, err
-			}
-		default:
-			return false, nil, nil, nil, errors.Errorf("Unsupported node type '%s' for node '%s' in deployment '%s'", nodeType, nodeName, deploymentID)
-		}
+	err = g.generateInstances(&instances, outputs, &cmdEnv)
+	if err != nil {
+		return false, nil, nil, nil, errors.Wrap(err, "Failed to generate instances")
 	}
 
 	jsonInfra, err := json.MarshalIndent(infrastructure, "", "  ")
@@ -138,4 +105,46 @@ func (g *awsGenerator) GenerateTerraformInfraForNode(ctx context.Context, cfg co
 
 	log.Debugf("Infrastructure generated for deployment with id %s", deploymentID)
 	return true, outputs, cmdEnv, nil, nil
+}
+
+func (g *awsGenerator) generateInstances(instances *[]string, outputs map[string]string, cmdEnv *[]string) error {
+	nodeType, err := deployments.GetNodeType(*g.ctx, g.deploymentID, g.nodeName)
+	if err != nil {
+		return err
+	}
+
+	for instNb, instanceName := range *instances {
+		instanceState, err := deployments.GetInstanceState(*g.ctx, g.deploymentID, g.nodeName, instanceName)
+		if err != nil {
+			return err
+		}
+		if instanceState == tosca.NodeStateDeleting || instanceState == tosca.NodeStateDeleted {
+			// Do not generate something for this node instance (will be deleted if exists)
+			continue
+		}
+
+		switch nodeType {
+		case "yorc.nodes.aws.Compute":
+			*instances, err = deployments.GetNodeInstancesIds(*g.ctx, g.deploymentID, g.nodeName)
+			if err != nil {
+				return err
+			}
+
+			err = g.generateAWSInstance(*g.ctx, *g.cfg, g.deploymentID, g.nodeName, instanceName, g.infrastructure, outputs, cmdEnv)
+			if err != nil {
+				return err
+			}
+		case "yorc.nodes.aws.PublicNetwork":
+			// Nothing to do
+		case "yorc.nodes.aws.EBSVolume":
+			err = g.generateEBS(instanceName, instNb)
+			if err != nil {
+				return err
+			}
+		default:
+			return errors.Errorf("Unsupported node type '%s' for node '%s' in deployment '%s'", nodeType, g.nodeName, g.deploymentID)
+		}
+	}
+
+	return nil
 }
